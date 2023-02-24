3189bbfcc329540a8ca439d089f8ebbca6a8a837
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-24 16:56:33 +0100

    update

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alert = ({ message, alertType }) => {
     return (
-        <div className=" flex items-center justify-center fixed top-0 left-0 h-full w-full bg-black bg-opacity-[90%]">
+        <div className=" flex items-center justify-center fixed top-0 left-0 h-full w-full bg-black bg-opacity-[90%] z-10">
             <div className='bg-white flex items-center justify-center py-10 px-5 gap-4 flex-col rounded-lg'>
                 <i class="ri-close-circle-line text-7xl text-red-500"></i>
                 <p className="text-center text-red-500 text-2xl py-1 px-2 mb-3">{message}</p>
--- client/src/pages/NewInvioce.jsx
+++ client/src/pages/NewInvioce.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Alert from '../components/Alert'
 
 const NewInvioce = () => {
+
+    const [clientName, setClientName] = useState("Frank")
+    const [clientEmail, setClientEmail] = useState("frank@gmail.com")
+    const [clientCounrty, setClientCountry] = useState("Nigeria")
+    const [clientCity, setClientCity] = useState("Anambra Awka")
+    const [clientStreetAddress, setClientStreetAddress] = useState("Ifite School Gate")
+    const [clientPostalCode, setClientPostalCode] = useState("14210")
+    const [invoiceDate, setInvoiceDate] = useState("")
+    const [paymentTerms, setPaymentTerms] = useState("")
+    const [productDescription, setProductDescription] = useState("Web Development")
+    const [error, setError] = useState("")
+    const [alertType, setAlertType] = useState("")
 
     const { vendorData } = useSelector((state) => state.vendorAuth)
     console.log(vendorData.vendor.streetAddress)
 
+    function handleClientDetailSubmission(e) {
+        e.preventDefault();
+        if (!clientName || !clientEmail || !clientCounrty || !clientCity || !clientStreetAddress || !clientPostalCode || !invoiceDate || !paymentTerms || !productDescription) {
+            setError("Update all fields")
+            setTimeout(() => {
+                setError("")
+            }, 2000)
+        }
+        console.log(clientName, clientEmail, clientCounrty, clientCity, clientStreetAddress, clientPostalCode, new Date(invoiceDate).toDateString(), paymentTerms, productDescription)
+    }
+
     return (
-        <div className="text-white mt-20 w-[50%] mx-auto justify-between gap-[5rem] bg-[#1F213A] p-8 rounded-md mb-10">
+        <div className="text-white mt-20 w-[50%] mx-auto justify-between gap-[5rem] bg-[#1F213A] p-8 rounded-md mb-10 relative">
+            {error && <Alert message={error} alertType={alertType} />}
             <h1 className='text-xl font-bold'>Create #RT3080</h1>
             <div className='mt-9'>
                 <p className='font-bold text-[#7B5EF8]'>Bill From</p>
@@ -35,64 +60,67 @@ const NewInvioce = () => {
                 </div>
             </div>
 
-            <div className='mt-20'>
-                <p className='font-bold text-[#7B5EF8]'>Bill To</p>
-                <label className="block my-7 w-full">
-                    <h1>Client's Name</h1>
-                    <input type="text" placeholder='Frank' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
-                </label>
-                <div className="block my-3 w-full">
-                    <h1>Client's Email</h1>
-                    <input type="text" placeholder='client@gmail.com' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
-                </div>
-                <div className='flex items-center gap-4'>
+            <form onSubmit={handleClientDetailSubmission}>
+                <div className='mt-20'>
+                    <p className='font-bold text-[#7B5EF8]'>Bill To</p>
+                    <label className="block my-7 w-full">
+                        <h1>Client's Name</h1>
+                        <input type="text" value={clientName} onChange={e => setClientName(e.target.value)} placeholder='Frank' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                    </label>
                     <div className="block my-3 w-full">
-                        <h1>Client's Country</h1>
-                        <input type="text" placeholder='Nigeria' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                        <h1>Client's Email</h1>
+                        <input type="text" value={clientEmail} onChange={e => setClientEmail(e.target.value)} placeholder='client@gmail.com' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
                     </div>
-                    <div className="block my-3 w-full">
-                        <h1>Client's City</h1>
-                        <input type="text" placeholder='Anambra Awka' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                    <div className='flex items-center gap-4'>
+                        <div className="block my-3 w-full">
+                            <h1>Client's Country</h1>
+                            <input type="text" value={clientCounrty} onChange={e => setClientCountry(e.target.value)} placeholder='Nigeria' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                        </div>
+                        <div className="block my-3 w-full">
+                            <h1>Client's City</h1>
+                            <input type="text" value={clientCity} onChange={e => setClientCity(e.target.value)} placeholder='Anambra Awka' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                        </div>
                     </div>
-                </div>
 
-                <div className='flex items-center gap-4 mt-1'>
-                    <div className="block my-3 w-full">
-                        <h1>Client's Street Address</h1>
-                        <input type="text" placeholder='Ifite School Gate' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                    <div className='flex items-center gap-4 mt-1'>
+                        <div className="block my-3 w-full">
+                            <h1>Client's Street Address</h1>
+                            <input type="text" value={clientStreetAddress} onChange={e => setClientStreetAddress(e.target.value)} placeholder='Ifite School Gate' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                        </div>
+                        <div className="block my-3 w-full">
+                            <h1>Client's Postal Code</h1>
+                            <input type="text" value={clientPostalCode} placeholder='141270' onChange={e => setClientPostalCode(e.target.value)} className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
+                        </div>
                     </div>
-                    <div className="block my-3 w-full">
-                        <h1>Client's Postal Code</h1>
-                        <input type="text" placeholder='141270' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
-                    </div>
-                </div>
 
-                <div className='flex items-center gap-4 mt-1'>
-                    <div className="block my-3 w-full">
-                        <h1>Invioce Date</h1>
-                        <input type="date" className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] px-3 py-2 w-full bg-[#141625]" />
+                    <div className='flex items-center gap-4 mt-1'>
+                        <div className="block my-3 w-full">
+                            <h1>Invioce Date</h1>
+                            <input type="date" value={invoiceDate} onChange={e => setInvoiceDate(e.target.value)} className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] px-3 py-2 w-full bg-[#141625]" />
+                        </div>
+                        <div className="block my-3 w-full">
+                            <h1>Payment Terms</h1>
+                            <select name="paymentTerms" value={paymentTerms} onChange={e => setPaymentTerms(e.target.value)} className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]">
+                                <option value="">-- Select Payment Terms --</option>
+                                <option value="Next 10 Days">Next 10 Days</option>
+                                <option value="Next 20 Days">Next 20 Days</option>
+                                <option value="Next 30 Days">Next 30 Days</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="block my-3 w-full">
-                        <h1>Payment Terms</h1>
-                        <input type="text" placeholder='Email' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
-                    </div>
-                </div>
 
-                <div className="block my-7 w-full">
-                    <h1>Product Description</h1>
-                    <input type="text" placeholder='Web Development' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
-                </div>
-            </div>
-            <div className="flex items-center justify-end gap-1">
-                <div className="flex items-center justify-between bg-[#141625] rounded-full gap-2 py-3 px-5 hover:cursor-pointer">
-                    <Link to='/' className='text-[12px]'>Cancel</Link>
-                </div>
-                <button>
-                    <div className="flex items-center justify-between bg-[#7B5EF8] rounded-full gap-2 py-3 px-5 hover:cursor-pointer">
-                        <p className='text-[12px]'>Save Changes</p>
+                    <div className="block my-7 w-full">
+                        <h1>Product Description</h1>
+                        <input type="text" value={productDescription} onChange={e => setProductDescription(e.target.value)} placeholder='Web Development' className="focus:outline-none border-gray-800 rounded-[4px] border-[1px] pl-3 py-2 w-full bg-[#141625]" />
                     </div>
-                </button>
-            </div>
+                </div>
+                <div className="flex items-center justify-end gap-1">
+                    <div className="flex items-center justify-between bg-[#141625] rounded-full gap-2 py-3 px-5 hover:cursor-pointer">
+                        <Link to='/' className='text-[12px]'>Cancel</Link>
+                    </div>
+                    <input type="submit" className="text-[12px] flex items-center justify-between bg-[#7B5EF8] rounded-full gap-2 py-3 px-5 hover:cursor-pointer" value="Save Changes and Continue" />
+                </div>
+            </form>
         </div>
     )
 }
